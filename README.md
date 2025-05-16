be13e87107dd86178a1efeee367841ebb70a6102
Mehul Jadhav <mehuljadhav@Mehuls-MacBook-Pro.local>
2025-05-16 17:18:12 +0530

    Base File

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Finndealz
+
+New Web Design Project <br>
+Started on 15th May - 2025 by Mehul and team<br>
+
+Figma : https://www.figma.com/design/t7m5buV3xFPrBe8QtRK9A3/AD501---Redesign--Gift-Card----Finndealz.com?node-id=3357-851&t=kmFJjwHKG3wM1d47-1 <br>
+
+● Remote Repository Link :- <br>
+git remote add origin https://github.com/SupporthivesProjects/Finndealz.git<br>
+
+
+● Reporting Manager :- Jeet Sir<br>
+
+
+● Team Members :- <br>
+↳ UI Team:- Mehul, Tirthak, Sakshi Kadam<br>
+↳ Integration Team:- <br>
+↳ QA Team:- Parth <br>
+
+
+● Project Leads :-<br>
+↳ UI Team:- Mehul<br>
+↳ Integration Team:- <br>
+↳ QA Team:- <br>
+
+
+● Pages Assigned :-<br>
+↳ Mehul:- Header, Footer, Home, My account, Account details, Order history, Order history details, Product Listing, Product Details<br>
+↳ Sakshi:- Terms & Conditions, Error, Login, Sign up, Reset password, Payment success, Payment failure<br>
+↳ Tirthak:- Checkout, Contact us, Sell Gift Card, About us
---
+++ assets/js/main.js
@@ -0,0 +1,127 @@
+$(document).ready(function () {
+    $(".hamburger_menu").click(function () {
+        $(".header_mobo_main_slide").fadeIn("slow");
+        $(".hamburger_menu").fadeOut("slow");
+        $(".hamburger_menu_close").fadeIn("slow");
+        $(".header_cart_mobo_main_slide").fadeOut("slow");
+    });
+    $(".hamburger_menu_close").click(function () {
+
+        if ($(".header_cart_mobo_main_slide").css("display") != "none") {
+            $(".header_cart_mobo_main_slide").fadeOut("slow");
+            $(".hamburger_menu").fadeIn("slow");
+            $(".hamburger_menu_close").fadeOut("slow");
+        } else {
+            $(".hamburger_menu").fadeIn("slow");
+            $(".header_mobo_main_slide").fadeOut("slow");
+            $(".hamburger_menu_close").fadeOut("slow");
+        }
+    });
+    $(".cart_menu").click(function () {
+        $(".header_cart_mobo_main_slide").fadeIn("slow");
+        $(".header_mobo_main_slide").fadeOut("slow");
+        $(".hamburger_menu_close").fadeIn("slow");
+        $(".hamburger_menu").fadeOut("slow");
+    });
+});
+
+function closeMobileCart() {
+    $(".header_cart_mobo_main_slide").fadeOut("slow");
+    $(".hamburger_menu_close").fadeOut("slow");
+    $(".hamburger_menu").fadeIn("slow");
+}
+
+function justDrop(droperId, roterId, element) {
+    const theId = document.getElementById(droperId);
+    const theId2 = document.getElementById(roterId);
+
+
+    if (theId.classList.contains('d-none')) {
+        theId.classList.remove('d-none');
+        element.classList.add("active")
+        theId2.style.rotate = '180deg';
+    } else {
+        theId.classList.add('d-none');
+        element.classList.remove("active")
+        theId2.style.rotate = '0deg';
+    }
+}
+
+
+function field_box_file() {
+    console.log('me');
+    document.getElementById('document').click();
+}
+$("#document").on("change", function (e) {
+    document.getElementById('upload_placeholder').innerHTML = e.target.files[0].name;
+})
+
+
+function togglePassword(input_id, input_eye_image) {
+    const inputField = document.getElementById(input_id);
+    const inputFieldType = inputField.getAttribute('type');
+    if (inputFieldType === "password") {
+        inputField.setAttribute('type', 'text');
+        input_eye_image.src = "./assets/img/open_eye.svg"
+    } else {
+        inputField.setAttribute('type', 'password');
+        input_eye_image.src = "./assets/img/close_eye.svg"
+    }
+}
+
+
+$(document).ready(function () {
+    $('#pds2').owlCarousel({
+        loop:false,
+        margin: 28,
+        nav:false,
+        dots: false,
+        responsive:{
+            0:{
+                loop: true,
+                items:1.2,
+                nav: true,
+                dots: true,
+                dotsEach: 1,
+                navText: [
+                    "<span class='btn btn_owl'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='27' viewBox='0 0 16 27' fill='none'><path d='M13.3135 24.627L1.99977 13.3132L13.3135 1.99954' stroke='#BA8B4A' stroke-width='4' stroke-linecap='round' stroke-linejoin='round'/></svg></span>",
+                    "<span class='btn btn_owl'><svg xmlns='http://www.w3.org/2000/svg' width='16' height='27' viewBox='0 0 16 27' fill='none'><path d='M2.68652 2L14.0002 13.3137L2.68652 24.6274' stroke='#BA8B4A' stroke-width='4' stroke-linecap='round' stroke-linejoin='round'/></svg></span>"
+                ],
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+})
+
+function addQty(inp_id) {
+    const x = document.getElementById(inp_id);
+    let currentValue = parseInt(x.value) || 0;
+    if (currentValue < 9) {
+        x.value = currentValue + 1;
+    }
+    
+}
+function subQty(inp_id) {
+    const x = document.getElementById(inp_id);
+    let currentValue = parseInt(x.value) || 0;
+    if (currentValue > 1) {
+        x.value = currentValue - 1;
+    }
+}
+
+// $(document).ready(function () {
+//     $(window).on('scroll', function () {
+//         const scrollTop = window.scrollY;
+//         if (scrollTop >= 90) {
+//             $("#header").css("background", "#162222")
+//         } else {
+//             $("#header").css("background", "transparent")
+//         }
+//     });
+// });
+
